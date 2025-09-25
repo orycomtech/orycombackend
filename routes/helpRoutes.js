3dd11c005bf5874eb5eb110fd373542cf5c85388
orycomtech <orycomtech@gmail.com>
2025-09-25 19:35:34 +0300

    Update helpRoutes.js

--- routes/helpRoutes.js
+++ routes/helpRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const { submitRequest, getAllRequests, updateStatus } = require("../controllers/helpController");
 const authenticate = require("../middleware/authenticate");
-const upload = require("../middleware/upload");
+const {upload} = require("../middleware/upload");
 const authenticateAdmin = require("../middleware/authenticateAdmin");
 
 // Student/Instructor submit request
